7d5f65849f6bd33bf75241ac13f0e8a76974cc8f
Aayush <mahapatraaayush@gmail.com>
2020-09-14 12:50:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,7 @@ const App = () => {
 
   useEffect(() => {
     firebase.initializeApp({
-      apiKey: 'AIzaSyD2iH_BLAlyKYRT7MYCtIx-XVxjedPPYzc',
-      authDomain: 'authentication-a7069.firebaseapp.com',
-      databaseURL: 'https://authentication-a7069.firebaseio.com',
-      projectId: 'authentication-a7069',
-      storageBucket: 'authentication-a7069.appspot.com',
-      messagingSenderId: '294312810281',
-      appId: '1:294312810281:web:4a3661aa23f111a7908938'
+      
     });
 
     firebase.auth().onAuthStateChanged(user => {
